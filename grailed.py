6690528d772352fa5f91db7eca21bac6d299243e
lucaskye <1902060@brebeuf.qc.ca>
2024-09-21 17:29:00 -0400

    Change name

--- grailed.py
+++ grailed.py
@@ -7,50 +7,49 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-base_url = "https://www.grailed.com/designers/maison-margiela"
+base_url = "https://www.grailed.com/designers/issey-miyake"
 
 chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless=new")
 
 
 def setup(options, url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
-    time = 30
+    time = 10
     wait = WebDriverWait(driver, time)
 
     try:
         wait.until(
             EC.visibility_of_element_located((By.XPATH, "//div[@class='feed-item']"))
         )
-        print("Page loaded")
         return driver
     except TimeoutException:
-        print("Timed out")
+        print("Timed out waiting for page to load")
         driver.quit()
         exit()
 
 
 def teardown(driver):
     driver.quit()
-    print("Closing browser")
+    print("\nClosing browser")
 
 
 def get_product_info(driver):
     try:
         product_info = driver.find_elements(By.XPATH, "//div[@class='feed-item']")
         product_info = [x.text for x in product_info]
-    
+
         product_info = [x.split("\n")[1:5] for x in product_info]
         for product in product_info:
             for field in product:
-                if "FREE SHIPPING" in field:
+                if "FREE SHIPPING" in field or "ago" in field or "See Similar" in field:
                     product.remove(field)
-                if "ago" in field:
-                    product.remove(field)
-                if "See Similar" in field:
-                    product.remove(field)
-                    
+            price = product[-1]
+            if "$" in price :
+                product[-1] = int(price.split("$")[1] )
+
         df = pd.DataFrame(
             product_info, columns=["brand", "size", "product_name", "price"]
         )
@@ -67,6 +66,16 @@ def main():
     try:
         # for _ in range(10):
         product_info = get_product_info(driver)
+        product_info.sort_values(by="price", inplace=True)
+
+        # gats = product_info.loc[
+        #     product_info["product_name"].str.contains(
+        #         "GAT|German Army Trainer", case=False, na=False
+        #     )
+        # ]
+        # Get rows with sizes of 8.5,9, or 9.5
+        # sizes = gats.loc[gats["size"].str.contains("8.5|9|9.5", case=False, na=False)]
+
         print(product_info)
 
     finally:
